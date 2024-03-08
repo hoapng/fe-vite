37c457ed222f26d2e675ea1a6a55e084572d7291
hoapng <giahoa01giathieu@gmail.com>
2024-03-08 19:38:22 +0700

    book detail 2

--- src/components/Book/ViewDetail.jsx
+++ src/components/Book/ViewDetail.jsx
@@ -8,67 +8,12 @@ import { BsCartPlus } from "react-icons/bs";
 import BookLoader from "./BookLoader";
 
 const ViewDetail = (props) => {
+  const { dataBook } = props;
+  console.log(dataBook);
   const [isOpenModalGallery, setIsOpenModalGallery] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const refGallery = useRef(null);
-
-  const images = [
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-      thumbnail: "https://picsum.photos/id/1018/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-      thumbnail: "https://picsum.photos/id/1015/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-    {
-      original: "https://picsum.photos/id/1019/1000/600/",
-      thumbnail: "https://picsum.photos/id/1019/250/150/",
-      originalClass: "original-image",
-      thumbnailClass: "thumbnail-image",
-    },
-  ];
 
   const handleOnClickImage = () => {
     //get current index onClick
@@ -93,77 +38,94 @@ const ViewDetail = (props) => {
         }}
       >
         <div style={{ padding: "20px", background: "#fff", borderRadius: 5 }}>
-          {/* <Row gutter={[20, 20]}>
-                        <Col md={10} sm={0} xs={0}>
-                            <ImageGallery
-                                ref={refGallery}
-                                items={images}
-                                showPlayButton={false} //hide play button
-                                showFullscreenButton={false} //hide fullscreen button
-                                renderLeftNav={() => <></>} //left arrow === <> </>
-                                renderRightNav={() => <></>}//right arrow === <> </>
-                                slideOnThumbnailOver={true}  //onHover => auto scroll images
-                                onClick={() => handleOnClickImage()}
-                            />
-                        </Col>
-                        <Col md={14} sm={24}>
-                            <Col md={0} sm={24} xs={24}>
-                                <ImageGallery
-                                    ref={refGallery}
-                                    items={images}
-                                    showPlayButton={false} //hide play button
-                                    showFullscreenButton={false} //hide fullscreen button
-                                    renderLeftNav={() => <></>} //left arrow === <> </>
-                                    renderRightNav={() => <></>}//right arrow === <> </>
-                                    showThumbnails={false}
-                                />
-                            </Col>
-                            <Col span={24}>
-                                <div className='author'>Tác giả: <a href='#'>Jo Hemmings</a> </div>
-                                <div className='title'>How Psychology Works - Hiểu Hết Về Tâm Lý Học</div>
-                                <div className='rating'>
-                                    <Rate value={5} disabled style={{ color: '#ffce3d', fontSize: 12 }} />
-                                    <span className='sold'>
-                                        <Divider type="vertical" />
-                                        Đã bán 6969</span>
-                                </div>
-                                <div className='price'>
-                                    <span className='currency'>
-                                        {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(696966666)}
-                                    </span>
-                                </div>
-                                <div className='delivery'>
-                                    <div>
-                                        <span className='left'>Vận chuyển</span>
-                                        <span className='right'>Miễn phí vận chuyển</span>
-                                    </div>
-                                </div>
-                                <div className='quantity'>
-                                    <span className='left'>Số lượng</span>
-                                    <span className='right'>
-                                        <button ><MinusOutlined /></button>
-                                        <input defaultValue={1} />
-                                        <button><PlusOutlined /></button>
-                                    </span>
-                                </div>
-                                <div className='buy'>
-                                    <button className='cart'>
-                                        <BsCartPlus className='icon-cart' />
-                                        <span>Thêm vào giỏ hàng</span>
-                                    </button>
-                                    <button className='now'>Mua ngay</button>
-                                </div>
-                            </Col>
-                        </Col>
-                    </Row> */}
-          <BookLoader />
+          {dataBook && dataBook?._id ? (
+            <Row gutter={[20, 20]}>
+              <Col md={10} sm={0} xs={0}>
+                <ImageGallery
+                  ref={refGallery}
+                  items={dataBook?.items}
+                  showPlayButton={false} //hide play button
+                  showFullscreenButton={false} //hide fullscreen button
+                  renderLeftNav={() => <></>} //left arrow === <> </>
+                  renderRightNav={() => <></>} //right arrow === <> </>
+                  slideOnThumbnailOver={true} //onHover => auto scroll dataBook?.items
+                  onClick={() => handleOnClickImage()}
+                />
+              </Col>
+              <Col md={14} sm={24}>
+                <Col md={0} sm={24} xs={24}>
+                  <ImageGallery
+                    ref={refGallery}
+                    items={dataBook?.items}
+                    showPlayButton={false} //hide play button
+                    showFullscreenButton={false} //hide fullscreen button
+                    renderLeftNav={() => <></>} //left arrow === <> </>
+                    renderRightNav={() => <></>} //right arrow === <> </>
+                    showThumbnails={false}
+                  />
+                </Col>
+                <Col span={24}>
+                  <div className="author">
+                    Tác giả: <a href="#">{dataBook?.author}</a>{" "}
+                  </div>
+                  <div className="title">{dataBook?.mainText}</div>
+                  <div className="rating">
+                    <Rate
+                      value={5}
+                      disabled
+                      style={{ color: "#ffce3d", fontSize: 12 }}
+                    />
+                    <span className="sold">
+                      <Divider type="vertical" />
+                      {dataBook?.sold}
+                    </span>
+                  </div>
+                  <div className="price">
+                    <span className="currency">
+                      {new Intl.NumberFormat("vi-VN", {
+                        style: "currency",
+                        currency: "VND",
+                      }).format(dataBook?.price ?? 0 - side)}
+                    </span>
+                  </div>
+                  <div className="delivery">
+                    <div>
+                      <span className="left-side">Vận chuyển</span>
+                      <span className="right-side">Miễn phí vận chuyển</span>
+                    </div>
+                  </div>
+                  <div className="quantity">
+                    <span className="left-side">{dataBook?.quantity}</span>
+                    <span className="right-side">
+                      <button>
+                        <MinusOutlined />
+                      </button>
+                      <input defaultValue={1} />
+                      <button>
+                        <PlusOutlined />
+                      </button>
+                    </span>
+                  </div>
+                  <div className="buy">
+                    <button className="cart">
+                      <BsCartPlus className="icon-cart" />
+                      <span>Thêm vào giỏ hàng</span>
+                    </button>
+                    <button className="now">Mua ngay</button>
+                  </div>
+                </Col>
+              </Col>
+            </Row>
+          ) : (
+            <BookLoader />
+          )}
         </div>
       </div>
       <ModalGallery
         isOpen={isOpenModalGallery}
         setIsOpen={setIsOpenModalGallery}
         currentIndex={currentIndex}
-        items={images}
+        items={dataBook?.items}
         title={"hardcode"}
       />
     </div>
--- src/pages/book/index.jsx
+++ src/pages/book/index.jsx
@@ -1,16 +1,62 @@
 import { useLocation } from "react-router-dom";
 import ViewDetail from "../../components/Book/ViewDetail";
+import { useEffect, useState } from "react";
+import { callFetchBookById } from "../../services/api";
 
 const BookPage = () => {
+  const [dataBook, setDataBook] = useState({});
+
   let location = useLocation();
   // console.log("location", location);
   let params = new URLSearchParams(location.search);
   const id = params?.get("id");
 
-  console.log("id", id);
+  // console.log("id", id);
+  useEffect(() => {
+    fetchBook(id);
+  }, [id]);
+  const fetchBook = async (id) => {
+    const res = await callFetchBookById(id);
+    if (res && res.data) {
+      let raw = res.data;
+      //process data
+      raw.items = getImages(raw);
+
+      setTimeout(() => {
+        setDataBook(raw);
+      }, 0);
+    }
+  };
+
+  const getImages = (raw) => {
+    const images = [];
+    if (raw.thumbnail) {
+      images.push({
+        original: `${import.meta.env.VITE_BACKEND_URL}/images/book/${
+          raw.thumbnail
+        }`,
+        thumbnail: `${import.meta.env.VITE_BACKEND_URL}/images/book/${
+          raw.thumbnail
+        }`,
+        originalClass: "original-image",
+        thumbnailClass: "thumbnail-image",
+      });
+    }
+    if (raw.slider) {
+      raw.slider?.map((item) => {
+        images.push({
+          original: `${import.meta.env.VITE_BACKEND_URL}/images/book/${item}`,
+          thumbnail: `${import.meta.env.VITE_BACKEND_URL}/images/book/${item}`,
+          originalClass: "original-image",
+          thumbnailClass: "thumbnail-image",
+        });
+      });
+    }
+    return images;
+  };
   return (
     <>
-      <ViewDetail />
+      <ViewDetail dataBook={dataBook} />
     </>
   );
 };
